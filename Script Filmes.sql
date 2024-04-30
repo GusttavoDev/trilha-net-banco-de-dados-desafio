ad9d810250a940e0307c7542f5ae87f99797d19f
GusttavoDev <71856143+GusttavoDev@users.noreply.github.com>
2024-04-30 17:25:54 -0300

    Update Script Filmes.sql

--- Script Filmes.sql
+++ Script Filmes.sql
@@ -325,3 +325,64 @@ REFERENCES [dbo].[Generos] ([Id])
 GO
 ALTER TABLE [dbo].[FilmesGenero] CHECK CONSTRAINT [FK__FilmesGen__IdGen__2E1BDC42]
 GO
+
+
+SELECT Nome, Ano
+FROM Filmes;
+
+SELECT Nome, Ano
+FROM Filmes
+ORDER BY Ano ASC;
+
+SELECT Nome, Ano, Duracao
+FROM Filmes
+WHERE Nome = 'De Volta para o Futuro';
+
+SELECT Nome, Ano
+FROM Filmes
+WHERE Ano = 1997;
+
+SELECT Nome, Ano
+FROM Filmes
+WHERE Ano > 2000;
+
+SELECT Nome, Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC;
+
+SELECT Nome, Duracao
+FROM Filmes
+WHERE Duracao > 100 AND Duracao < 150
+ORDER BY Duracao ASC;
+
+SELECT Ano, COUNT(*) AS Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+SELECT Ano, COUNT(*) AS Quantidade
+FROM Filmes
+GROUP BY Ano
+ORDER BY Quantidade DESC;
+
+SELECT PrimeiroNome, UltimoNome
+FROM Atores
+WHERE Genero = 'F'
+ORDER BY PrimeiroNome;
+
+SELECT f.Nome AS NomeFilme, g.Nome AS Genero
+FROM Filmes f
+INNER JOIN FilmesGenero fg ON f.IdFilme = fg.IdFilme
+INNER JOIN Generos g ON fg.IdGenero = g.IdGenero;
+
+SELECT f.Nome AS NomeFilme, g.Nome AS Genero
+FROM Filmes f
+INNER JOIN FilmesGenero fg ON f.IdFilme = fg.IdFilme
+INNER JOIN Generos g ON fg.IdGenero = g.IdGenero
+WHERE g.Nome = 'Mistério';
+
+SELECT f.Nome AS NomeFilme, a.PrimeiroNome, a.UltimoNome, ef.Papel
+FROM Filmes f
+INNER JOIN ElencoFilme ef ON f.IdFilme = ef.IdFilme
+INNER JOIN Atores a ON ef.IdAtor = a.IdAtor;
